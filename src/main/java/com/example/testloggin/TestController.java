631e5fe5bbbbfdc0a06d5c7572d24107e24848ea
saysun <sunayong@gmail.com>
2020-05-14 14:00:33 +0000

    test from gitpod

--- src/main/java/com/example/testloggin/TestController.java
+++ src/main/java/com/example/testloggin/TestController.java
@@ -17,4 +17,9 @@ public class TestController {
     public String getTesty(@RequestParam String date, @RequestParam int num){
         return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
     }
+
+    @GetMapping("/gitpoding")
+    public String gitpod(){
+        return "Git pod is really a awesome tool. This is a game changer!!!!";
+    }
 }
